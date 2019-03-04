2820cff09ea7423902b0a4c5d1b9fddcf9b1f818
Aman Kumar <ak47su30ac@gmail.com>
2019-03-04 18:54:18 +0530

    improved front-end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
 
   <!-- Area Chart -->
-  <div class="col-xl-8 col-lg-7">
+  <div class="col-xl-9 col-lg-8">
     <div class="card shadow mb-4">
       <!-- Card Header - Dropdown -->
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -19,7 +19,19 @@
   </div>
 
   <!-- Pie Chart -->
-  <div class="col-xl-4 col-lg-5">
+  <div class="col-xl-3 col-lg-4">
+    <div class="col-xl-12 col-md-12 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Points</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">P 215</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
     <div class="card shadow mb-4">
       <!-- Card Header - Dropdown -->
       <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">

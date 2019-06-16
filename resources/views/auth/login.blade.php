8d79ed453afe80427aaadf549d4488dcc69065e6
Aman Kumar <ak47su30ac@gmail.com>
2019-06-16 18:42:56 +0530

    disabled fb and github login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,12 +54,12 @@
                     <a href="{{ url('/login/google') }}" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
                     </a>
-                    <a href="{{ url('/login/facebook') }}" class="btn btn-facebook btn-user btn-block">
+                    <!-- <a href="{{ url('/login/facebook') }}" class="btn btn-facebook btn-user btn-block">
                       <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                     </a>
                     <a href="{{ url('/login/github') }}" class="btn btn-github btn-user btn-block">
                       <i class="fab fa-github-f fa-fw"></i> Login with Github
-                    </a>
+                    </a> -->
                   </form>
                 </div>
               </div>
